a5f324e1c7aa89ca29d06e604112ad76281e2369
enricoferrer <enrico.santana@sptech.school>
2026-03-19 14:07:56 -0300

    feat: aluno pode ser criado sem uma turma

--- app/models/aluno.py
+++ app/models/aluno.py
@@ -12,8 +12,8 @@ class Aluno(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
     matricula = Column(String, nullable=False, unique=True)
     data_matricula = Column(Date, nullable=False)
-    fk_turma = Column(UUID(as_uuid=True), ForeignKey("turmas.id", ondelete="NO ACTION", onupdate="NO ACTION"))
+    fk_turma = Column(UUID(as_uuid=True), ForeignKey("turmas.id", ondelete="NO ACTION", onupdate="NO ACTION"), nullable=True)
     fk_usuario = Column(UUID(as_uuid=True), ForeignKey("usuarios.id", ondelete="NO ACTION", onupdate="NO ACTION"), nullable=False)
     
-    turma = relationship("Turma", back_populates="alunos", foreign_keys=[fk_turma])
-    usuario = relationship("Usuario", back_populates="alunos", foreign_keys=[fk_usuario])
+    turma = relationship("Turma", back_populates="aluno", foreign_keys=[fk_turma])
+    usuario = relationship("Usuario", back_populates="aluno", foreign_keys=[fk_usuario])
