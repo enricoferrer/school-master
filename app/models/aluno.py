b4eeba2a978efde682752afbd0b6f6475f615e57
enricoferrer <enrico.santana@sptech.school>
2026-03-19 13:17:55 -0300

    feat: matricula do Aluno agora é UNICA

--- app/models/aluno.py
+++ app/models/aluno.py
@@ -10,7 +10,7 @@ class Aluno(Base):
     __tablename__ = "alunos"
     
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
-    matricula = Column(String, nullable=False)
+    matricula = Column(String, nullable=False, unique=True)
     data_matricula = Column(Date, nullable=False)
     fk_turma = Column(UUID(as_uuid=True), ForeignKey("turmas.id", ondelete="NO ACTION", onupdate="NO ACTION"))
     fk_usuario = Column(UUID(as_uuid=True), ForeignKey("usuarios.id", ondelete="NO ACTION", onupdate="NO ACTION"), nullable=False)
