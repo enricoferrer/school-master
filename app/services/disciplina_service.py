a07e557822eb994d00e334e5de3a61b347dab20c
enricoferrer <enrico.santana@sptech.school>
2026-03-22 14:48:35 -0300

    refac: fluxo de disciplina assincrono

--- app/services/disciplina_service.py
+++ app/services/disciplina_service.py
@@ -9,21 +9,21 @@ class DisciplinaService:
     def __init__(self, repository: DisciplinaRepository):
         self.repository = repository
     
-    def create(self, data: DisciplinaCreate):
-        disciplina_existente = self.repository.get_disciplina_by_codigo(data.codigo)
+    async def create(self, data: DisciplinaCreate):
+        disciplina_existente = await self.repository.get_disciplina_by_codigo(data.codigo)
         if disciplina_existente:
             raise DuplicateFieldException(f"Disciplina com o código '{data.codigo}' já existe!")
-        return self.repository.create(data)
+        return await self.repository.create(data)
     
-    def list_disciplina(self):
-        return self.repository.list_disciplina()
+    async def list_disciplina(self):
+        return await self.repository.list_disciplina()
     
-    def get_disciplina_by_id(self, id: UUID):
-        disciplina = self.repository.get_disciplina_by_id(id)
+    async def get_disciplina_by_id(self, id: UUID):
+        disciplina = await self.repository.get_disciplina_by_id(id)
         if disciplina is None:
             raise NotFoundException(f"Disciplina com o ID: '{id}' não foi encontrada!")
         return disciplina
     
-    def delete_disciplina_by_id(self, id: UUID):
-        disciplina = self.get_disciplina_by_id(id)
-        self.repository.delete_disciplina(disciplina)
+    async def delete_disciplina_by_id(self, id: UUID):
+        disciplina = await self.get_disciplina_by_id(id)
+        await self.repository.delete_disciplina(disciplina)
