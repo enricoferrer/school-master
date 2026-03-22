ec7ae92eb709a438b02ef8dfb4990e3d44a53f48
enricoferrer <enrico.santana@sptech.school>
2026-03-22 14:48:15 -0300

    refac: fluxo de professor assincrono

--- app/repositories/professor_repository.py
+++ app/repositories/professor_repository.py
@@ -1,25 +1,30 @@
-from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
 from app.schemas.professor import ProfessorCreate
 from app.models.professor import Professor
 
 class ProfessorRepository:
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
         self.db = db
         
-    def create(self, data: ProfessorCreate):
+    async def create(self, data: ProfessorCreate):
         professor = Professor(**data.model_dump())
         self.db.add(professor)
-        self.db.commit()
-        self.db.refresh(professor)
+        await self.db.commit()
+        await self.db.refresh(professor)
         return professor
     
-    def list_all_professor(self):
-        return self.db.query(Professor).all()
+    async def list_all_professor(self):
+        result = await self.db.execute(select(Professor))
+        return result.scalars().all()
     
-    def get_professor_by_id(self, id: UUID):
-        return self.db.query(Professor).filter(Professor.id == id).first()
+    async def get_professor_by_id(self, id: UUID):
+        result = await self.db.execute(
+            select(Professor).where(Professor.id == id)
+        )
+        return result.scalar_one_or_none()
     
-    def delete_professor(self, professor: Professor):
-        self.db.delete(professor)
-        self.db.commit()
+    async def delete_professor(self, professor: Professor):
+        await self.db.delete(professor)
+        await self.db.commit()
